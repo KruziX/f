70c40a147580f9c9fcb7203193f68d0af26898dd
KruziX <90509328+KruziX@users.noreply.github.com>
2022-05-20 22:01:57 +0300

    Update Boom.py

--- Boom.py
+++ Boom.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-@borg.on(events.NewMessage(pattern=r"\.bum", outgoing=True))
+@borg.on(events.NewMessage(pattern=r"\.boom", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
