c4a9463b378647adecdc0c4cf8defa1c7aa00c80
KruziX <90509328+KruziX@users.noreply.github.com>
2022-05-20 21:10:43 +0300

    Create Boom.py

---
+++ Boom.py
@@ -0,0 +1,30 @@
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.bum", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+ 
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.7)
+    await event.edit("🧨🧨🧨🧨🧨🧨🧨🧨 \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.7)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n🧨🧨🧨🧨🧨🧨🧨🧨 \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.7)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n🧨🧨🧨🧨🧨🧨🧨🧨 \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.7)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n🧨🧨🧨🧨🧨🧨🧨🧨 \n▪️▪️▪️▪️▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.7)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n🧨🧨🧨🧨🧨🧨🧨🧨 \n")
+    await asyncio.sleep(1)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n💥💥💥💥💥💥💥💥 \n")
+    await asyncio.sleep(0.7)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n💥💥💥💥💥💥💥💥 \n💥💥💥💥💥💥💥💥 \n")
+    await asyncio.sleep(0.7)
+    await event.edit("▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n▪️▪️▪️▪️▪️▪️▪️▪️ \n😵😵😵😵😵😵😵😵 \n")
+   
