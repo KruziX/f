d63e87d3ed589c3c02e269834c017be8f308aaad
KruziX <90509328+KruziX@users.noreply.github.com>
2022-05-21 08:58:23 +0300

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -1,7 +1,16 @@
 from telethon import events
+from .. import loader, utils
+from datetime import timedelta
+from telethon import functions
+from telethon.tl.types import Message
 import asyncio
 import os
 import sys
+import random
+
+@loader.tds
+class Tetris Animation(loader.Module):
+    """При использовании команды, Вы получите красивую анимацию тетриса, которая соберёт вам хорошую фигуру"""
 
 
 @borg.on(events.NewMessage(pattern=r"\.tetris", outgoing=True))
