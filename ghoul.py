e616bdd00f78f661f45a4a41e5efede6e071d2b8
KruziX <90509328+KruziX@users.noreply.github.com>
2022-05-25 18:35:42 +0300

    Create ghoul.py

---
+++ ghoul.py
@@ -0,0 +1,17 @@
+from .. import loader
+
+class gylMod(loader.Module):
+	strings = {"name": "IamGhole/@KruzProjects"}
+	
+	async def watcher(self, message):
+		me = (await message.client.get_me())
+		if message.sender_id == me.id:
+			if message.text.lower() == "я гуль":
+				a = 1000				
+				while a != 6:
+					c = str(a)
+					a = a - 7
+					b = str(a)
+					d = c + " - 7 = " + b
+					await message.respond(d)
+				await message.respond("Теперь понял кто тут гуль? 😈")
